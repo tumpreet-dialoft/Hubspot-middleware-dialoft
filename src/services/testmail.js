90959324deea485b388a4eeddd7b22837c20cfca
tumpreet singh <stumpreet@gmail.com>
2026-02-24 02:54:13 +0530

    Test Module Deleted

--- src/services/testmail.js
+++
@@ -1,7 +0,0 @@
-
-const { sendSMS } = require('./smsService')
-
-
-
-//sendFollowupEmail('tumpreet@dialoftai.com', 'tumpreet', 2, 'https://cal.com/romanlbinder/ai-discovery-call');
-sendSMS('+916280505657', 'Hi ${name}, sorry we missed you! Id love to chat. You can book a time here: [LINK]' );
